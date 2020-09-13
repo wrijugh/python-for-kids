cb2d4117a781ec787d10495f4f8f41fe8b978bb2
Wriju Ghosh <wriju@hotmail.com>
2020-09-13 14:41:21 +0530

    This is through bash Sun 09/13/2020-14:41:21.25

--- g.bat
+++ g.bat
@@ -1,5 +1,5 @@
-echo "\n================\n Git: add -> commit -> push \n==================\n"
-echo "================================="
+echo "\n================\n \Git: add -> commit -> push \n==================\n"
+
 
 git add *
 git commit -m "This is through bash %date%-%time%"
