55a8c8d3f811676263a7e56eab0b4061fc8a22ce
Wriju Ghosh <wriju@hotmail.com>
2020-09-17 19:00:41 +0530

    A BAT file committed on Thu 09/17/2020-19:00:41.28

--- SampleCode/string_video.py
+++ SampleCode/string_video.py
@@ -1,10 +1,6 @@
-print("Hello")
 
-print(10 > 8) # True
-print(10 > 11) # False
 
-#You can even assign a value to a variable and it would become boolean
-
-bln = (10 > 9)
-print(type(bln)) # <class, 'bool'>
+x = (10 == 10)
+print(x)
+print(type(x))
 
