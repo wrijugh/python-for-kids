d35aea714219ebbb639b842e71683d8bcd7bf22f
Wriju Ghosh <wriju@hotmail.com>
2020-09-29 18:31:46 +0530

    A BAT file committed on Tue 09/29/2020-18:31:46.50

---
+++ SampleCode/16UserInput.py
@@ -0,0 +1,15 @@
+# Basic calculator with variables but fixed values 
+
+a=10 #variable but fixed values
+b=20 #variable but fixed values
+
+result = a + b 
+
+print(result)
+
+# Input 
+num1 = input("Enter the first number:")
+num2 = input("Enter the second number:")
+
+result = num1+num2 
+print(result)
