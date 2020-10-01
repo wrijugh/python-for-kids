810d41679ce33d7751bf1c370db64ecdce2b2c7d
Wriju Ghosh <wriju@hotmail.com>
2020-10-01 11:20:23 +0530

    A BAT file committed on Thu 10/01/2020-11:20:23.41

--- SampleCode/19Calculator.py
+++ SampleCode/19Calculator.py
@@ -1,3 +1,32 @@
 
 class Calculator:
+    def __init__(self, num1, num2):
+        self.a = num1
+        self.b = num2
+
+    # addition
+    def addition(self):
+        result = self.a + self.b
+        print("Addition:        " + str(result))
     
+    # subtraction
+    def subtraction(self):
+        result = self.a - self.b 
+        print("Subtraction:     " + str(result))
+
+    # multiplication
+    def multiplication(self):
+        result = self.a * self.b
+        print("Multiplication:  " + str(result))
+    
+    # division
+    def division(self):
+        result = self.a / self.b 
+        print("Division:        " + str(result))
+
+#Calling the class 
+mycalc = Calculator(20, 10)
+mycalc.addition()
+mycalc.subtraction()
+mycalc.multiplication()
+mycalc.division()
