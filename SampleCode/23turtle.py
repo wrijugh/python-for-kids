f6faa7e71cb901bbc6e423071443e013a0fd8679
Wriju Ghosh <wriju@hotmail.com>
2020-11-29 10:56:31 +0530

    A BAT file committed on Sun 11/29/2020-10:56:31.33

--- SampleCode/23turtle.py
+++ SampleCode/23turtle.py
@@ -1,8 +1,13 @@
 import turtle
+import time
+
+time.sleep(2)
 turtle.color("red", "yellow")
-
 turtle.begin_fill()
-turtle.circle(120)
-turtle.end_fill()
 
+for i in range(18):    
+    turtle.circle(120)
+    turtle.right(20)
+
+turtle.end_fill()
 turtle.done()
